40d5adc78a594065eb348712b04624a9c758cc0b
santiago ss <santyss427@gmail.com>
2023-09-06 08:30:23 -0500

    Se actualiza el conversor de monedas

--- conversor.py
+++ conversor.py
@@ -8,12 +8,13 @@ def Conversor():
 1 - Pesos Colombianos a Dólares
 2 - Dólares a Pesos Colombianos
 3 - Finalizar conversor
+
     """
 
     # Función para convertir Pesos Colombianos a Dólares
     def pesos_a_dolares():
         pesos = float(input("\n ¿Cuántos Pesos Colombianos tienes?: "))
-        valor_dolar = 1 / 4086
+        valor_dolar = 1 / 4095
         dolares = pesos * valor_dolar
         dolares = round(dolares, 2)
         print(f"Tienes ${dolares} Dólares")
@@ -21,13 +22,13 @@ def Conversor():
     # Función para convertir Dólares a Pesos Colombianos
     def dolares_a_pesos():
         dolares = float(input("¿Cuántos Dólares tienes?: "))
-        valor_pesos = 4086  
+        valor_pesos = 4095  
         pesos = dolares * valor_pesos
         print(f"Tienes ${pesos} Pesos Colombianos")
 
     while True:
         print(menu)
-        opcion = input("Elige una opción (1/2/3): ")
+        opcion = input("Elige una opción 1 / 2 / 3: ")
 
         if opcion == '1':
             pesos_a_dolares()
